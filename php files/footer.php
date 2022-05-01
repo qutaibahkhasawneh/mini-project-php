1578745f93c0a55b0668cdf29b3ee361cae6fff2
qutaibahkhasawneh <100359595+qutaibahkhasawneh@users.noreply.github.com>
2022-05-01 16:23:35 +0300

    main

---
+++ php files/footer.php
@@ -0,0 +1,43 @@
+<div class=" mt-5">
+
+  <footer class="text-center text-lg-start" style="background-color: black;">
+    <div class="container d-flex justify-content-center py-5">
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+        <i class="fab fa-facebook-f"></i>
+      </button>
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+        <i class="fab fa-youtube"></i>
+      </button>
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+        <i class="fab fa-instagram"></i>
+      </button>
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+        <i class="fab fa-twitter"></i>
+      </button>
+
+
+      
+    </div>
+    <div class=" nav navbar-nav1  d-flex justify-content-center">
+                    <a href="viewProducts.php" class=" text-white btn btn3 mx-4">HOME</a>
+                    <a href="admin_page.php" class="text-white btn btn3 mx-2">ADD PRODUCTS</a>
+                    <a href="#" class=" text-white btn btn3 mx-4 ">ABOUT</a>
+                    <!-- <a href="#" class="btn btn3">CONTACT</a>
+                    <a href="#" class="btn btn2">Account</a> -->
+                    <!-- <a href="#" class="btn btn2"><i class="fa fa-search"></i></a>
+                    <a href="#" class="btn btn2"><i class="fa fa-shopping-cart"></i></a> -->
+                </div>
+
+    <!-- Copyright -->
+    <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2022 Copyright
+     
+    </div>
+    <!-- Copyright -->
+    <script></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+  </footer>
+  
+</div>
+</body>
+</html>
---
+++ php files/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+    <style>
+#cont{ width: 600px;
+
+}
+@media only screen and (max-width: 1025px) {
+  #cont { width: 600px;
+
+  }
+}
+
+
+
+        @media only screen and (max-width: 576px) {
+  #cont { width: 400px;
+
+  }
+  .size{ height: 0.5em;
+         width: 0.5em;   
+  }
+}
+
+
+
+
+    </style>
+</head>
+<body>
+<header style="background-color:black">
+        <nav class=" navbar navbar-expand-md navbar-light ">
+            <div class="container">
+              <a class="navbar-brand" href="admin_page.php"><img src="images/logo.png" style="width: 100px; height: 50px;"></a>
+
+                <div class="nav navbar-nav1">
+                    <a href="viewProducts.php" class=" text-white btn btn3 mx-4">HOME</a>
+                    <a href="admin_page.php" class="text-white btn btn3 mx-4">ADD PRODUCTS</a>
+                    <a href="#" class=" text-white btn btn3 mx-">ABOUT</a>
+                    <!-- <a href="#" class="btn btn3">CONTACT</a>
+                    <a href="#" class="btn btn2">Account</a> -->
+                    <!-- <a href="#" class="btn btn2"><i class="fa fa-search"></i></a>
+                    <a href="#" class="btn btn2"><i class="fa fa-shopping-cart"></i></a> -->
+                </div>
+
+            </div>
+        </nav>
+    </header>
